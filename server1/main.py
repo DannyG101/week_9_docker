94ea3f28eb7d6e957c445007e374a556450acb61
Danny G <dannygoldberg13@gmail.com>
2025-12-17 12:49:05 +0200

    built server1 + docker file + updated command.md

---
+++ server1/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+from pathlib import Path
+import uvicorn
+
+app = FastAPI()
+
+DB_PATH = Path("db/shopping_list.json")
+
+class Item(BaseModel):
+    name: str
+    quantity: int
+
+
+def check_database_exists():
+    if not DB_PATH.exists():
+        print(f"ERROR: Database does not exist")
+        raise FileNotFoundError(f"Database does not exist: {DB_PATH}")
+
+
+def load_database():
+    with open(DB_PATH, "r") as f:
+        return json.load(f)
+
+def save_database(data):
+    with open(DB_PATH, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+@app.get("/items")
+async def list_items():
+    db = load_database()
+    return db
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+
+    check_database_exists()
+
+    db = load_database()
+    item_id = len(db) + 1
+    current_item = {"id": item_id, "name": item.name, "quantity": item.quantity}
+    db.append(current_item)
+    save_database(db)
+    return {
+        "message": "Item added successfully",
+        "item_id": item_id,
+        "item": current_item
+            }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
